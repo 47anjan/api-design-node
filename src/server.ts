4112e3bc1c02b8cc076ef3d5fbb1234c42124b0c
47anjan <anjankarmakar15@gmail.com>
2025-10-17 12:19:01 +0600

    add middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import express from 'express'
 
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
 import authRoutes from './routes/authRoutes.ts'
 import userRoutes from './routes/userRoutes.ts'
 import habitRoutes from './routes/habitRoutes.ts'
+import { isTest } from '../env.ts'
 
 const app = express()
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+  morgan('dev', {
+    skip: () => isTest(),
+  })
+)
 
 app.get('/health', (req, res) => {
   res.json({ message: 'hello world' }).status(200)
