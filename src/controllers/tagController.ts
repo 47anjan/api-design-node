9bb2e30895744f34d4447fd3557b78bb6119a851
47anjan <anjankarmakar15@gmail.com>
2025-10-25 22:18:59 +0600

    add create tag route

--- src/controllers/tagController.ts
+++ src/controllers/tagController.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from 'express'
 import db from '../db/connection.ts'
 import { tags } from '../db/schema.ts'
+import { eq } from 'drizzle-orm'
 
 export const getTags = async (req: Request, res: Response) => {
   try {
@@ -12,5 +13,37 @@ export const getTags = async (req: Request, res: Response) => {
   } catch (error) {
     console.log({ error })
     res.json({ error: 'Failed to get tags' })
+  }
+}
+
+export const createTag = async (req: Request, res: Response) => {
+  try {
+    const { name, color } = req.body
+
+    const existingTag = await db.query.tags.findFirst({
+      where: eq(tags.name, name),
+    })
+
+    if (existingTag) {
+      return res
+        .status(409)
+        .json({ error: 'Tag with this name already exists' })
+    }
+
+    const [newTag] = await db
+      .insert(tags)
+      .values({
+        name,
+        color: color || '#6B7280',
+      })
+      .returning()
+
+    res.status(201).json({
+      message: 'Tag created successfully',
+      tag: newTag,
+    })
+  } catch (error) {
+    console.log({ error })
+    res.json({ error: 'Failed to create tag' })
   }
 }
--- src/routes/tagRoutes.ts
+++ src/routes/tagRoutes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express'
-import { getTags } from '../controllers/tagController.ts'
+import { createTag, getTags } from '../controllers/tagController.ts'
+import z, { string } from 'zod'
+import { validateBody } from '../middleware/validation.ts'
 
 const router = Router()
 
+const createTagSchema = z.object({
+  name: string().min(3),
+  color: string().optional(),
+})
+
 router.get('/', getTags)
+router.post('/', validateBody(createTagSchema), createTag)
 
 export default router
